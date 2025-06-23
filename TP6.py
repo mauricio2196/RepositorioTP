036c282ae1fede6f3c75b2102cac6a31947ee987
Mauri21 <lauremauricio17@gmail.com>
2025-06-23 18:31:59 -0300

    Diccionario prueba TP6.py

---
+++ TP6.py
@@ -0,0 +1,52 @@
+
+
+
+Datos = {  
+
+    "Alumnos": ["alumno1","alumno2","alumno3"],
+    
+    1 : {
+        "Nombre" : "Camila",
+        "Apellido" : "Fernandez",
+        "DNI"   : 44333556,
+        "Fecha-Nacimiento": "12/04/2017",
+        "Tutor" : "Fransico Fernandez",
+        "Notas" : {"Matematica": 8, "Lengua":7, "Artistica":10},
+        "Faltas" : 2,
+        "Amonestaciones" : "Ninguna"
+    },
+    
+    2 : {
+        "Nombre" : "Manuel",
+        "Apellido" : "Molina",
+        "DNI"   : 44994335,
+        "Fecha-Nacimiento": "06/08/2017",
+        "Tutor" : "Fransico Fernandez",
+        "Notas" : {"Matematica": 6, "Lengua":8, "Artistica":8},
+        "Faltas" : 3,
+        "Amonestaciones" : "Ninguna",
+    },
+
+    3 : {
+        "Nombre" : "Luisana",
+        "Apellido" : "Sabedra",
+        "DNI"   : "44322333",
+        "Fecha-Nacimiento": "18/10/2017",
+        "Tutor" : "Fransico Fernandez",
+        "Notas" : {"Matematica": 10, "Lengua":5, "Artistica":7},
+        "Faltas" : 6,
+        "Amonestaciones" : "Ninguna",
+    },
+
+
+
+      }
+
+
+for clave in Datos:
+    print(Datos["Alumnos"][2]["DNI"])
+    
+            
+        
+   
+
